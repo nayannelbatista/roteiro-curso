1272293b84f60f50c15a140e11a1c173f0097bff
nayannelbatista <nayannelopes.bd@gmail.com>
2022-06-01 18:09:39 -0300

    Criação do formulário

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,17 @@
+import { Pensamento } from './../pensamento';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
